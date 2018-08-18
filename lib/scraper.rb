9c892d946ae42c0e589ca6ca1c4a5aa1dbdd1d57
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 22:24:07 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require_relative './course.rb'
 
 class Scraper
 
-    def get_page      
+    def get_page
       page=Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     end
 
