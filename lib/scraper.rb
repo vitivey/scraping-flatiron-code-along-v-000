15bc057038423241b8b1bf6e75047b8197c822b6
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 22:45:04 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,7 @@ class Scraper
         course.schedule= post.css("em").text
         course.description=post.css("p").text
       end
-      binding.pry
+      # binding.pry
     end
 
     def get_courses
