a3ffd2c125a85ae2cb49815d97b1768a1a0d5535
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 22:50:04 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,13 +9,7 @@ class Scraper
     def get_page
       page=Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
-      page.css(".post").each do |post|
-        course = Course.new
-        course.title= post.css("h2").text
-        course.schedule= post.css(".date").text
-        course.description=post.css("p").text
-      end
-      binding.pry
+      
     end
 
 
